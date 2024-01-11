33d7f162cb01e85a18a856d9343a2dd67eb7f5f7
michaelmensa <michaelmensa@live.com>
2024-01-11 10:13:28 +0000

    task6[i]

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,14 +10,14 @@ def hash_password(password: str) -> bytes:
     ''' function that expects one string arg and
     returns a salted, hashed password
     '''
-    password = password.encode() 
+    password = password.encode()
     hashed = hashpw(password, bcrypt.gensalt())
     return hashed
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     ''' function that validates password '''
-    password = password.encode() 
+    password = password.encode()
     if checkpw(password, hashed_password):
         return True
     else:
