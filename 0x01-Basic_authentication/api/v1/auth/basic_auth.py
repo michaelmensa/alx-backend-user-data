d141c29d0b5f16f0192242c1ee76d9f553fab8c9
michaelmensa <michaelmensa@live.com>
2024-01-15 23:52:26 +0000

    task10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,8 @@ Module api/v1/auth/basic_auth.py: basic authentication
 '''
 from api.v1.auth.auth import Auth
 import base64
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -52,3 +53,22 @@ class BasicAuth(Auth):
             return user_email, password
         else:
             return None, None
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        ''' returns the User instance based on his email and password '''
+        if user_email is None or type(user_email) != str:
+            return None
+        if user_pwd is None or type(user_pwd) != str:
+            return None
+
+        try:
+            users = User.search({'email': user_email})
+            if not users or users == []:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+            return None
+        except Exception:
+            return None
