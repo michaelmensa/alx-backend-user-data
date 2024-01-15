c200ba022da2a6cd0178927c1e5104c9722dbaf4
michaelmensa <michaelmensa@live.com>
2024-01-15 22:49:00 +0000

    task8

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Module api/v1/auth/basic_auth.py: basic authentication
 '''
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -20,4 +21,18 @@ class BasicAuth(Auth):
             base64_cred = authorization_header.split(' ')[-1]
             return base64_cred
         else:
+            return None
+    
+    def decode_base64_authorization_header(self,
+                                           authorization_header: str) -> str:
+        '''
+        returns the decoded base64 part of the authorization header for
+        a basic authentication '''
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+
+        try:
+            decoded = base64.b64decode(authorization_header)
+            return decoded.decode('utf-8')
+        except base64.binascii.Error:
             return None
