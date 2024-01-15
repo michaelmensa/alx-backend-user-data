6eea666d203244d6a1892818589ecad0610eac4e
michaelmensa <michaelmensa@live.com>
2024-01-15 22:32:07 +0000

    task7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,15 @@ class BasicAuth(Auth):
     ''' class BasicAuth that inherits from Auth.
     class handles basic authentication
     '''
+    def extract_base64_authorization_header(self,
+            authorization_header: str) -> str:
+        '''
+        returns the base64 part of the authorization header for
+        a basic authentication '''
+        if authorization_header is None or type(authorization_header) != str:
+            return None
+        if authorization_header.startswith('Basic '):
+            base64_cred = authorization_header.split(' ')[-1]
+            return base64_cred
+        else:
+            return None
