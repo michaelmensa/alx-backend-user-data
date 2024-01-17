522aea9a74493d7b0103512022dfee7e371b4fb5
michaelmensa <michaelmensa@live.com>
2024-01-17 15:23:26 +0000

    task5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,11 +34,13 @@ def auth_handler():
     excluded_paths = [
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/'
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/'
             ]
     if auth:
         if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None:
+            if auth.authorization_header(request) is None and \
+                    auth.session_cookie(request) is None:
                 abort(401, description='Unauthorized')
             if auth.current_user(request) is None:
                 abort(403, description='Forbidden')
