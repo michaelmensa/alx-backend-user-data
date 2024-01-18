ce76951cd14ed986e8d298311adec8f4eb063668
michaelmensa <michaelmensa@live.com>
2024-01-18 13:02:39 +0000

    task9: more headaches[i]

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -23,10 +23,8 @@ class SessionExpAuth(SessionAuth):
     def create_session(self, user_id=None):
         ''' method that creates session with user_id '''
         # create session ID by calling super()
-        if user_id:
-            session_id = super().create_session(user_id)
-            if type(session_id) != str:
-                return None
+        session_id = super().create_session(user_id)
+        if session_id:
             # store the session dictionary in user_id_by_session_id
             self.user_id_by_session_id[session_id] = {
                 'user_id': user_id,
@@ -39,15 +37,13 @@ class SessionExpAuth(SessionAuth):
     def user_id_for_session_id(self, session_id=None):
         ''' method that gets user id from session id '''
         # return None is session ID is None
-        if session_id:
-            if session_id in self.user_id_by_session_id:
-                session_dictionary = self.user_id_by_session[session_id]
-                if self.session_duration <= 0:
-                    return session_dictionary['user_id']
-                if 'created_at' not in session_dictionary:
-                    return None
-                timespan = timedelta(seconds=self.session_duration)
-                exp = session_dictionary['created_at'] + timespan
-                if exp < datetime.now():
-                    return None
-                return session_dictionary['user_id']
+        if session_id is None or session_id not in self.user_id_by_session_id:
+            return None
+        session_dictionary = self.user_id_by_session_id[session_id]
+        if self.session_duration <= 0 or 'created_at' not in session_dict:
+            return None
+        timespan = timedelta(seconds=self.session_duration)
+        exp = session_dictionary['created_at'] + timespan
+        if exp < datetime.now():
+            return None
+        return session_dict['user_id']
