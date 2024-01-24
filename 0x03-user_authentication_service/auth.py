e73c2ef5b09c43f7123ef88505c74eb3b03e165c
michaelmensa <michaelmensa@live.com>
2024-01-24 15:59:40 +0000

    update auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -103,8 +103,9 @@ class Auth():
         use reset_token to find user. raise ValueError is user not exist
         Otherwise, hash password and update user password and reset_token
         field to None'''
-        user = self._db.find_user_by(reset_token=reset_token)
-        if not user:
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            user.hashed_password = _hash_password(password)
+            self._db.update_user(user.id, reset_token=None)
+        except NoResultFound:
             raise ValueError
-        user.hashed_password = _hash_password(password)
-        self._db.update_user(user.id, reset_token=None)
